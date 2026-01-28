20870d7f4334b859c3eab57e35caeaa6d96c96e6
pazachildplays <pazachildplays@gmail.com>
2026-01-27 20:36:13 -0600

    Fix: Convert API files to ES6 export for Vercel serverless functions

--- api/config.js
+++ api/config.js
@@ -49,7 +49,7 @@ async function getConfig() {
   }
 }
 
-module.exports = async (req, res) => {
+export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
